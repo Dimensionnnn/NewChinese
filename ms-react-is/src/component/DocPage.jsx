7385c9671c951a45e7300f9fb59507e8ebec2e4f
scx_glb <scx_glb@163.com>
2022-07-18 11:41:15 +0800

    change public private

--- ms-react-is/src/component/DocPage.jsx
+++ ms-react-is/src/component/DocPage.jsx
@@ -20,69 +20,82 @@ import {
 import "../App.css";
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
 
-const searchClient = instantMeiliSearch("http://127.0.0.1:7700/", "MASTER_KEY")
+// const searchClient = instantMeiliSearch("http://127.0.0.1:7700/", "MASTER_KEY")
 const client = new MeiliSearch({ host: "http://127.0.0.1:7700", apiKey: "MASTER_KEY" });
 export default class DocPage extends Component {
   state = {
-    curIndex: 1
+    curIndex: 20
   }
   setWaitPublicCheck = (hit) => {// 私有或公有加入待审核
     //加入到待审核index，同时需要携带该用户的userid，以便后续限制此用户只能访问用户id是自己的数据
     // hit数据中加入userid
-    client.index('wait_to_check').addDocuments([{
-      id: hit.id,
-      url: hit.url,
-      title: hit.title,
-      text: hit.text,
-      public: 'false',
-      userid: 'admin' //后续根据当前登录用户进行修改
-    }])
-    this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
-    window.location.reload()
+    if (window.confirm('确定公开该条吗？管理员通过后将从私有index删除，可在公有库搜索', hit.title, '，若存在则为审核通过')) {
+      client.index('wait_to_check').addDocuments([{
+        id: hit.id,
+        url: hit.url,
+        title: hit.title,
+        text: hit.text,
+        public: 'false',
+        userid: 'admin' //后续根据当前登录用户进行修改
+      }])
+      this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
+      this.setState({ curIndex: this.state.curIndex - 1 })// 防止页面缓存不加载新加入的待审核
+      // window.location.reload()
+    }
   }
   setWaitPrivateCheck = (hit) => {// 私有或公有加入待审核
     //加入到待审核index，同时需要携带该用户的userid，以便后续限制此用户只能访问用户id是自己的数据
     // hit数据中加入userid
-    client.index('wait_to_check').addDocuments([{
-      id: hit.id,
-      url: hit.url,
-      title: hit.title,
-      text: hit.text,
-      public: "true",
-      userid: 'admin' //后续根据当前登录用户进行修改
-    }])
-    this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
-    window.location.reload()
+    if (window.confirm('确定私有该条吗？管理员通过后将从公有index删除，可在私有库搜索', hit.title, '，若存在则为审核通过')) {
+      client.index('wait_to_check').addDocuments([{
+        id: hit.id,
+        url: hit.url,
+        title: hit.title,
+        text: hit.text,
+        public: "true",
+        userid: 'admin' //后续根据当前登录用户进行修改
+      }])
+      this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
+      this.setState({ curIndex: this.state.curIndex - 1 })// 防止页面缓存不加载新加入的待审核
+      // window.location.reload()
+    }
   }
   setPrivate = (hit) => {// 通过私有化申请
-    client.index('all_private').addDocuments([{
-      id: hit.id,
-      url: hit.url,
-      title: hit.title,
-      text: hit.text,
-      public: "false",
-      userid: 'admin' //后续根据当前登录用户进行修改
-    }])
-    //设为私有从此index删除
-    client.index('wait_to_check').deleteDocument(hit.id)
-    client.index('doc_wiki_05').deleteDocument(hit.id)
-    this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
-    window.location.reload()
+    if (window.confirm('确定通过私有申请吗？通过后将从公有index删除，可在私有index搜索', hit.title, '查找私有后的信息')) {
+      client.index('all_private').addDocuments([{
+        id: hit.id,
+        url: hit.url,
+        title: hit.title,
+        text: hit.text,
+        public: "false",
+        userid: 'admin' //后续根据当前登录用户进行修改
+      }])
+      //设为私有从此index删除
+      client.index('wait_to_check').deleteDocument(hit.id)
+      setTimeout(this.setState({ curIndex: this.state.curIndex + 1 }), 2000);// 防止页面缓存不加载新加入的待审核
+      client.index('doc_wiki_05').deleteDocument(hit.id)
+      setTimeout(this.setState({ curIndex: this.state.curIndex - 1 }), 2000);// 防止页面缓存不加载新加入的待审核
+      
+      // window.location.reload()
+    }
   }
   setPublic = (hit) => {// 通过公有化申请 （未实现公开到哪个index，还需选择）
-    client.index('doc_wiki_05').addDocuments([{
-      id: hit.id,
-      url: hit.url,
-      title: hit.title,
-      text: hit.text,
-      public: "true",
-      userid: 'admin' //后续根据当前登录用户进行修改
-    }])
-    //设为私有从此index删除
-    client.index('wait_to_check').deleteDocument(hit.id)
-    client.index('all_private').deleteDocument(hit.id)
-    this.setState({ curIndex: this.state.curIndex + 1 })// 防止页面缓存不加载新加入的待审核
-    window.location.reload()
+    if (window.confirm('确定公开吗？公开后将从私有index与待审核index删除，可在公开库搜索', hit.title, '查找公开后的数据')) {
+      client.index('doc_wiki_05').addDocuments([{
+        id: hit.id,
+        url: hit.url,
+        title: hit.title,
+        text: hit.text,
+        public: "true",
+        userid: 'admin' //后续根据当前登录用户进行修改
+      }])
+      //设为私有从此index删除
+      client.index('wait_to_check').deleteDocument(hit.id)
+      setTimeout(this.setState({ curIndex: this.state.curIndex + 1 }), 2000);// 防止页面缓存不加载新加入的待审核
+      client.index('all_private').deleteDocument(hit.id)
+      setTimeout(this.setState({ curIndex: this.state.curIndex - 1 }), 1000);// 防止页面缓存不加载新加入的待审核
+      // window.location.reload()
+    }
   }
   updateDocIndexs = () => {
     this.props.updateIndexs('puQ8edwLae5cf8cd8b9c91c1f22ab17756e1f973dd621cd172e939e192a8908f5d99e4d2');
@@ -96,16 +109,16 @@ export default class DocPage extends Component {
         navigate(path, { state: { value: hit.text } });
       };
       return (
-        <div key={hit.id}>
+        <div key={hit.id+hit.title}>
           {
             displayedAttributes.map((attribute) => {
               return (
                 attribute === "public" ?
-                  <div className="hit-description">
+                  <div key={hit.id+attribute} className="hit-description">
                     已公开：
                     {hit.public === 'true' ? '是' : '否'}
                   </div> :
-                  <div className="hit-description">
+                  <div className="hit-description" key={hit.id+attribute}>
                     {attribute}:
                     <Snippet attribute={attribute} hit={hit} />
                   </div>
@@ -127,8 +140,7 @@ export default class DocPage extends Component {
       );
     };
     return (
-
-      <InstantSearch indexName={selectedIndex} searchClient={searchClient}>
+      <InstantSearch indexName={selectedIndex} searchClient={instantMeiliSearch("http://127.0.0.1:7700/", "MASTER_KEY")}>
         <div className="left-panel">
           <button onClick={this.updateDocIndexs} class="btn btn-default">加载indexs</button>
           <IndexList indexs={indexs} setIndex={setIndex} />
@@ -143,11 +155,11 @@ export default class DocPage extends Component {
           <CurrentRefinements />
           <SearchBox />
           <HitsPerPage
-            defaultRefinement={20}
+            defaultRefinement={this.state.curIndex}
             items={[
-              { value: 10, label: "显示 10 条每页" },
-              { value: 20, label: "显示 20 条每页" },
-              { value: 40, label: "显示 40 条每页" },
+              { value: this.state.curIndex - 10, label: "显示 10 条每页" },
+              { value: this.state.curIndex, label: "显示 20 条每页" },
+              { value: this.state.curIndex + 20, label: "显示 40 条每页" },
             ]}
           />
           <Hits hitComponent={Hit} />
