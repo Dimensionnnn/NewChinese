53274634e070ee1278474074e0b3ebc79873121b
smell-iron <254810106@qq.com>
2022-08-08 21:53:46 +0800

    Add icec to repo

--- ms-react-is/src/component/AnalyzePage.jsx
+++ ms-react-is/src/component/AnalyzePage.jsx
@@ -1,66 +1,11 @@
-import React, { Component, useState  } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-
-
-// class AnalyzePage extends Component {
-//     state = {
-//         text_to_analyze: ""
-//     }
-//     handeChange(item, event) {
-//         for (let StateItem in this.state) {
-//             if (item === StateItem) {
-//                 this.state[item] = event.target.value;
-//                 this.setState(this.state)
-//             }
-//         }
-//     }
-//     routeChange = () => {
-//         let navigate = useNavigate();
-//         let path = `/result`;
-//         navigate(path, {
-//             state: {
-//                 userid: this.props.userid,
-//                 text_to_analyze: this.state.text_to_analyze,
-//                 value: this.state.text_to_analyze,
-//                 hit: { userid: "admin", title: "null", text: this.state.text_to_analyze },
-//                 selectedIndex: "doc_wiki_05"
-//             }
-//         });
-//     };
-//     render() {
-//         return (
-//             <div>
-//                 <div className="input-box">
-//                     <label htmlFor="" ></label><input type="text" onChange={this.handeChange.bind(this, "text_to_analyze")} placeholder='请输入' value={this.state.text_to_analyze} />
-//                 </div>
-//                 <button onClick={this.routeChange} className="btn btn-default">分析文本</button>
-//             </div>
-//         );
-//     }
-// }
-
-// export default AnalyzePage;
-
-// import React from "react";
-import { useLocation } from 'react-router-dom';
 import { Button } from '@chakra-ui/react';
 
 
 const AnalyzePage = (props) => {
-    // let text_to_analyze = '';
     const [text_to_analyze, setText_to_analyze] = useState('');
     let navigate = useNavigate();
-    const location = useLocation();
-    // const handeChange = (event) => {
-    //     // for (let StateItem in this.state) {
-    //     //     if (item === StateItem) {
-    //     //         this.state[item] = event.target.value;
-    //     //         this.setState(this.state)
-    //     //     }
-    //     // }
-    //     // console.log(item)
-    //     text_to_analyze=event.target.value
-    // }
     const routeChange = () => {
         let path = `/result`;
         navigate(path, {
@@ -73,8 +18,6 @@ const AnalyzePage = (props) => {
             }
         });
     };
-
-
 
     return (
         <div style={{ textAlign: "center" }}>
