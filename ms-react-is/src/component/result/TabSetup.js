e0c257c7522f492182cbf671e907f197916e93bf
Linlin <707661163@qq.com>
2022-08-26 18:34:54 +0800

    add tab to analyze

--- ms-react-is/src/component/result/TabSetup.js
+++ ms-react-is/src/component/result/TabSetup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './tabIndex.css'
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 class TabSetup extends React.Component {
     constructor(props) {
@@ -30,17 +31,74 @@ class TabSetup extends React.Component {
                     <div className="m-sys-content">
                         <div className={"m-sys-view " + (tabActiveIndex === 0 ? 'active' : '')}>
                             <div style={{
-                                width: '100%',
+                                width: '85%',
                                 // height: '100 %',
                                 border: '2px solid #900',
-                                padding: '5px',
+                                padding: '3px',
+                                alignItems: 'center'
                                 // margin: 'auto'
                             }}>
-                                <p style={{fontSize:'14px'}}>分析结果： 适合HSK 6级以上的学习者</p>
+                                <p style={{fontSize:'16px'}}>分析结果： 适合HSK 6级以上的学习者</p>
                             </div>
                         </div>
                         <div className={"m-sys-view " + (tabActiveIndex === 1 ? 'active' : '')}>
-                            1
+                            <table id="table-to-xls" border="1" style={{ margin: '0 auto', width: '500px', height: ' 70px',fontSize:'16px' }}>
+                                <thead>
+                                    <tr>
+                                        <th>级别</th>
+                                        <th >1</th>
+                                        <th>2</th>
+                                        <th>3</th>
+                                        <th>4</th>
+                                        <th>5</th>
+                                        <th>6</th>
+                                        <th>7</th>
+                                        <th>8</th>
+                                        <th>9</th>
+                                        <th>未录入</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>词数</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>占比</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button style={{fontSize: '16px',padding:0}}>
+                                <ReactHTMLTableToExcel
+                                    id="test-table-xls-button"
+                                    className="download-table-xls-button"
+                                    table="table-to-xls"
+                                    filename="tablexls"
+                                    sheet="tablexls"
+                                    buttonText="下载分析明细" />
+                            </button>
+                            
+                            
                         </div>
                         <div className={"m-sys-view " + (tabActiveIndex === 2 ? 'active' : '')}>
                             2
