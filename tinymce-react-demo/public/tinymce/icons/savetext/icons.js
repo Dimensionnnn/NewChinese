9518b619b18556dcf03b1a3e7e549d3a6462bc39
Linlin <707661163@qq.com>
2022-08-27 13:20:18 +0800

    tiny backup

--- tinymce-react-demo/public/tinymce/icons/savetext/icons.js
+++ tinymce-react-demo/public/tinymce/icons/savetext/icons.js
@@ -2,5 +2,6 @@ import Tinymce from 'tinymce'
 Tinymce.IconManager.add('savetext', {
   icons: {
     'save': '<svg class="icon" width="200" height="200" viewBox="0 0 1024 1024"><path d="M925.2 357L667.1 98.7A64 64 0 00621.8 80H144a64 64 0 00-64 64v736a64 64 0 0064 64h736a64 64 0 0064-64V402.2a64 64 0 00-18.8-45.3zM288 144h192v112H288V144zm448 736H288V736h448v144zm144 0h-80V704a32 32 0 00-32-32H256a32 32 0 00-32 32v176h-80V144h80v144a32 32 0 0032 32h256a32 32 0 0032-32V144h77.8L880 402.2V880z"/></svg>',
+    // 'save': '<svg width="24" height="24">< path d="M5 16h14a2 2 0 012 2v2a2 2 0 01-2 2H5a2 2 0 01-2-2v-2c0-1.1.9-2 2-2zm0 2v2h14v-2H5zm10 0h2v2h-2v-2zm-4-6.4L8.7 9.3a1 1 0 10-1.4 1.4l4 4c.4.4 1 .4 1.4 0l4-4a1 1 0 10-1.4-1.4L13 11.6V4a1 1 0 00-2 0v7.6z"fill- rule="nonzero" /></svg> '
   }
 });
